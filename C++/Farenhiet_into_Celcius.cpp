1b2c4d8443178a42556b1a7aed39179e1133461a
Chandan <109679579+I-am-CHANDAN@users.noreply.github.com>
2022-10-13 11:04:04 +0530

    Add files via upload

---
+++ C++/Farenhiet_into_Celcius.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int f;
+   float c;
+   cout<<"Enter the Temperature in Farenhiet :\n";
+   cin>>f;
+
+   c=5.0/9.0*(f-32.0);
+
+   cout<<"Equivalent Celcius Temperature is :\n"<<c;
+   
+return 0;
+}       
---
+++ C++/Sum_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+    int a,b,sum;
+    cout<<"Enter the Value of a :\n";
+    cin>>a;
+
+    cout<<"Enter the value of b :\n";
+    cin>>b;
+
+    sum=a+b;
+    cout<<"Sum of value is :"<<sum;
+}
