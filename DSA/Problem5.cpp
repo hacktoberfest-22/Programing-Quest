8de2180b355d2ca7ea5aee6631145af28cad9cdc
Amit <amit0162.be21@chitkara.edu.in>
2022-10-13 20:25:59 +0530

    Adding some more files in dsa folder

---
+++ DSA/Problem5.cpp
@@ -0,0 +1,45 @@
+// Find the maximum product of two integers in an array
+
+// A naive solution is to consider every pair of elements and calculate their product. Update the maximum product found so far if the product of the current pair is greater. Finally, print the elements involved in the maximum product. This is demonstrated below in C, Java, and Python:
+
+#include <stdio.h>
+#include <limits.h>
+ 
+// A naive solution to finding the maximum product of two integers
+// in an array
+void findMaximumProduct(int arr[], int n)
+{
+    // base case
+    if (n < 2) {
+        return;
+    }
+ 
+    int max_product = INT_MIN;
+    int max_i, max_j;
+ 
+    // consider every pair of elements
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            // update the maximum product if required
+            if (max_product < arr[i] * arr[j])
+            {
+                max_product = arr[i] * arr[j];
+                max_i = i, max_j = j;
+            }
+        }
+    }
+ 
+    printf("Pair is (%d, %d)", arr[max_i], arr[max_j]);
+}
+ 
+int main()
+{
+    int arr[] = { -10, -3, 5, 6, -2 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    findMaximumProduct(arr, n);
+ 
+    return 0;
+}
